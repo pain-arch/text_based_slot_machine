cad8a3c9f2e1824a83e777b0e65802e5acec004b
pain-arch <himulol123@gmail.com>
2024-04-04 04:35:38 +0600

    Add deposit and number_of_lines functions

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 5
+
+
 def deposit():
     while True:
         amount = input("Enter the amount you want to deposit: $")
@@ -12,5 +17,23 @@ def deposit():
 
     return amount 
 
-deposit_amount = deposit()
-print("you have deposited $", deposit_amount,"to your account.")
+def number_of_lines():
+    while True:
+        lines = input("Enter the number of Lines you want to play (1 - "+str(MAX_LINES) +"):")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
+                break
+            else:
+                print("the number of the lines should be between 1 and " + str(MAX_LINES))
+        else:
+            print("Invalid number of lines, please entere a valid number of lines.")
+    return lines  
+
+def main():
+    balance = deposit()
+    lines = number_of_lines()
+
+    print("you have deposited $", balance , "and you are playing", lines, "lines.")
+
+main()                      
